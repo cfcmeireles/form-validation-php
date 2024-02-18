85d449659fc199a1f9e7d5aec10f38f98b455e4e
Carina Meireles <carinacmeireles@gmail.com>
2024-02-18 17:02:27 +0000

    add error handling

--- form-validation.php
+++ form-validation.php
@@ -3,30 +3,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_input = $_POST['email'];
     $sanitized_email = filter_var($email_input, FILTER_SANITIZE_EMAIL);
     if (!$email_input) {
-        echo "<div>Please make sure to fill out the email field</div>";
-    } else if (filter_var($sanitized_email, FILTER_VALIDATE_EMAIL)) {
-    } else {
-        echo "<div>Invalid email address</div>";
+        $errors[] = "Please make sure to fill out the email field";
+    } else if (!filter_var($sanitized_email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email address";
     }
     $subject_input = $_POST['subject'];
     if (!$subject_input) {
-        echo "<div>Please make sure to fill out the subject field</div>";
+        $errors[] = "Please make sure to fill out the subject field";
     }
     $message_input = $_POST['message'];
     if (!$message_input) {
-        echo "<div>Please make sure to fill out the message field</div>";
+        $errors[] = "Please make sure to fill out the message field";
     }
-    $sanitized_subject = htmlspecialchars($subject_input);
-    $sanitized_message = htmlspecialchars($message_input);
 
+    if (empty($errors)) {
+        $sanitized_subject = htmlspecialchars($subject_input);
+        $sanitized_message = htmlspecialchars($message_input);
 
-    $to = 'carinacmeireles@gmail.com';
-    $subject = $sanitized_subject;
-    $message = $sanitized_message;
-    $headers = "From: $sanitized_email";
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully";
+        $to = 'carinacmeireles@gmail.com';
+        $subject = $sanitized_subject;
+        $message = $sanitized_message;
+        $headers = "From: $sanitized_email";
+        if (mail($to, $subject, $message, $headers)) {
+            echo "Email sent successfully";
+        } else {
+            echo "Failed to send email";
+        }
     } else {
-        echo "Failed to send email";
+        foreach ($errors as $error) {
+            echo "<div>$error</div>";
+        }
     }
 }
